b0c5f9f638f449b65ecd7ea7bcd1670e986ffe2f
Evgeniy <jdev03sgo@gmail.com>
2023-04-16 21:13:56 +0300

    final, etc sex - vlad problem

--- index.js
+++ index.js
@@ -72,18 +72,3 @@ document.querySelector('.form__button').addEventListener('click', e => {
 document.querySelector('.form__button').addEventListener('click', e =>{
   location.href = './pages/form.html'
 })
-
-document.querySelector('.start-screen__button').addEventListener('click', e =>{
-  location.href = './index.html#tarifs'
-})
-
-document.querySelectorAll('.tarif__button').forEach(e =>{
-  e.addEventListener('click', e =>{
-    window.location.href = './pages/form2.html'
-  })
-})
-
-
-document.querySelector('.form__submit-button').addEventListener('click', e =>{
-
-})
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.form__submit-button')
+document.querySelectorAll('.form__submit-button').forEach(element => {
+  element.addEventListener('click', e =>{
+  location.href = './ty.html';
+})});
